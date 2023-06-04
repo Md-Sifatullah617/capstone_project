dcf4cfdb1b1b91e74e4dbec5754337fa01dc8829
Md. Sifatullah <sifatullah35-617@diu.edu.bd>
2023-06-04 22:09:54 +0600

    dsf

--- lib/Screens/Dashboard/dashboard.dart
+++ lib/Screens/Dashboard/dashboard.dart
@@ -21,15 +21,18 @@ class _UserDashboardState extends State<UserDashboard> {
     final url = Uri.parse("https://medex.com.bd/generics");
     final response = await http.get(url);
     dom.Document html = dom.Document.html(response.body);
-
-    final titles = html
-        .querySelectorAll(
-            "#ms-block > section > div > div:nth-child(2) > div:nth-child(1) > a:nth-child(1) > div")
-        .map((elements) => elements.innerHtml.trim())
-        .toList();
-    print("Count: ${titles.length}");
-    for (final title in titles) {
-      print("Titles: $title");
+    try {
+      final titles = html
+          .querySelectorAll(
+              "#ms-block > section > div > div:nth-child(2) > div:nth-child(1) > a:nth-child(1) > div > div.col-xs-12.data-row-top.dcind-title")
+          .map((elements) => elements.innerHtml.trim())
+          .toList();
+      print("Count: ${titles.length}");
+      for (final title in titles) {
+        print("Titles: $title");
+      }
+    } catch (e) {
+      print("Error: $e");
     }
   }
 
