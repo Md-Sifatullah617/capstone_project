40fac3abb20a3d7637a969db7738d58eed42d256
Md. Sifatullah <sifatullah35-617@diu.edu.bd>
2023-06-04 21:28:07 +0600

    sfd

--- lib/Screens/Dashboard/dashboard.dart
+++ lib/Screens/Dashboard/dashboard.dart
@@ -1,47 +1,21 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_auth/Screens/Login/login_screen.dart';
 import 'package:flutter_auth/components/navbar.dart';
 import 'package:http/http.dart' as http;
 import 'package:html/dom.dart' as dom;
 
-class UserDashboard extends StatelessWidget {
-   UserDashboard({Key? key}) : super(key: key);
+class UserDashboard extends StatefulWidget {
+  UserDashboard({Key? key}) : super(key: key);
 
-  final List<Widget> navItem = [
-    TextButton(
-        onPressed: () {},
-        child: const Text(
-          "Home",
-          style: TextStyle(color: Colors.black),
-        )),
-    TextButton(
-        onPressed: () {},
-        child: const Text(
-          "Profile",
-          style: TextStyle(color: Colors.black),
-        )),
-    TextButton(
-        onPressed: () {},
-        child: const Text(
-          "Request Medicine",
-          style: TextStyle(color: Colors.black),
-        )),
-    TextButton(
-        onPressed: () {},
-        child: const Text(
-          "Sent Medicine",
-          style: TextStyle(color: Colors.black),
-        )),
-  ];
+  @override
+  State<UserDashboard> createState() => _UserDashboardState();
+}
 
-  //list of icon for each navItem
-  final List<Icon> navIcon = [
-    const Icon(Icons.home),
-    const Icon(Icons.account_circle),
-    const Icon(Icons.medical_services),
-    const Icon(Icons.medical_services_outlined),
-  ];
+class _UserDashboardState extends State<UserDashboard> {
+  @override
+  void initState() {
+    getWebsiteData();
+    super.initState();
+  }
 
   Future getWebsiteData() async {
     final url = Uri.parse("https://medex.com.bd/generics");
@@ -53,53 +27,17 @@ class UserDashboard extends StatelessWidget {
             "#ms-block > section > div > div:nth-child(2) > div:nth-child(1) > a:nth-child(1) > div")
         .map((elements) => elements.innerHtml.trim())
         .toList();
+    print("Count: ${titles.length}");
+    for (final title in titles) {
+      print("Titles: $title");
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(
-        child: ListView(
-          children: <Widget>[
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text(
-                'Drawer Header',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                ),
-              ),
-            ),
-            //show the navItem list with a different prefix icon for each item
-            ...navItem.map((e) => ListTile(
-                  contentPadding: const EdgeInsets.only(right: 100, left: 20),
-                  title: e,
-                  leading: navIcon[navItem.indexOf(e)],
-                )),
-            const SizedBox(
-              height: 100,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: InkWell(
-                  onTap: () async {
-                    Navigator.pushAndRemoveUntil(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const LoginScreen()),
-                        (route) => false);
-                    await FirebaseAuth.instance.signOut();
-                  },
-                  child: const NavBar().btnNice()),
-            )
-          ],
-        ),
-      ),
       body: Stack(children: [
-        const NavBar(),
+        NavBar(),
         Container(
           margin: const EdgeInsets.only(top: 100),
           child: const Center(
--- lib/components/navbar.dart
+++ lib/components/navbar.dart
@@ -2,73 +2,153 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_auth/Screens/Login/login_screen.dart';
 import 'package:flutter_auth/responsive.dart';
-import '../Screens/Dashboard/dashboard.dart';
 
 class NavBar extends StatelessWidget {
-  const NavBar({Key? key}) : super(key: key);
+  NavBar({Key? key}) : super(key: key);
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
+  final List<Widget> navItem = [
+    TextButton(
+        onPressed: () {},
+        child: const Text(
+          "Home",
+          style: TextStyle(color: Colors.black),
+        )),
+    TextButton(
+        onPressed: () {},
+        child: const Text(
+          "Profile",
+          style: TextStyle(color: Colors.black),
+        )),
+    TextButton(
+        onPressed: () {},
+        child: const Text(
+          "Request Medicine",
+          style: TextStyle(color: Colors.black),
+        )),
+    TextButton(
+        onPressed: () {},
+        child: const Text(
+          "Sent Medicine",
+          style: TextStyle(color: Colors.black),
+        )),
+  ];
+
+  //list of icon for each navItem
+  final List<Icon> navIcon = [
+    const Icon(Icons.home),
+    const Icon(Icons.account_circle),
+    const Icon(Icons.medical_services),
+    const Icon(Icons.medical_services_outlined),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 40),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  width: 40,
-                  height: 40,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(18),
-                      gradient: const LinearGradient(colors: [
-                        Color(0xFFC86DD7),
-                        Color(0xFF3023AE),
-                      ], begin: Alignment.bottomRight, end: Alignment.topLeft)),
-                  child: const Center(
-                    child: Text("M",
-                        style: TextStyle(
-                            fontSize: 30,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold)),
+    return Scaffold(
+      key: _scaffoldKey,
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Drawer Header',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            //show the navItem list with a different prefix icon for each item
+            ...navItem.map((e) => ListTile(
+                  contentPadding: const EdgeInsets.only(right: 100, left: 20),
+                  title: e,
+                  leading: navIcon[navItem.indexOf(e)],
+                )),
+            const SizedBox(
+              height: 100,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: InkWell(
+                  onTap: () async {
+                    Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const LoginScreen()),
+                        (route) => false);
+                    await FirebaseAuth.instance.signOut();
+                  },
+                  child: btnNice()),
+            )
+          ],
+        ),
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 40),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: 40,
+                    height: 40,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(18),
+                        gradient: const LinearGradient(
+                            colors: [
+                              Color(0xFFC86DD7),
+                              Color(0xFF3023AE),
+                            ],
+                            begin: Alignment.bottomRight,
+                            end: Alignment.topLeft)),
+                    child: const Center(
+                      child: Text("M",
+                          style: TextStyle(
+                              fontSize: 30,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold)),
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                width: 10,
-              ),
-              const Text("Med_X", style: TextStyle(fontSize: 26))
-            ],
-          ),
-          if (!Responsive.isMobile(context))
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                ...UserDashboard().navItem,
                 const SizedBox(
-                  width: 20,
+                  width: 10,
                 ),
-                InkWell(
-                    onTap: () async {
-                      Navigator.pushAndRemoveUntil(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const LoginScreen()),
-                          (route) => false);
-                      await FirebaseAuth.instance.signOut();
-                    },
-                    child: btnNice())
+                const Text("Med_X", style: TextStyle(fontSize: 26))
               ],
-            )
-          else
-            IconButton(
-                onPressed: () {
-                  Scaffold.of(context).openDrawer();
-                },
-                icon: const Icon(Icons.menu, color: Colors.black)),
-        ],
+            ),
+            if (!Responsive.isMobile(context))
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  ...navItem,
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  InkWell(
+                      onTap: () async {
+                        Navigator.pushAndRemoveUntil(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const LoginScreen()),
+                            (route) => false);
+                        await FirebaseAuth.instance.signOut();
+                      },
+                      child: btnNice())
+                ],
+              )
+            else
+              IconButton(
+                  onPressed: () {
+                    _scaffoldKey.currentState?.openDrawer();
+                  },
+                  icon: const Icon(Icons.menu, color: Colors.black)),
+          ],
+        ),
       ),
     );
   }
